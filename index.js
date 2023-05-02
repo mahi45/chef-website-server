fc0a3cab4dbc6670354d05fe352bc5ce3e604778
Mohidul Hasan <mahi110145@gmail.com>
2023-05-03 05:09:08 +0600

    Recipe categories json has been created

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+const recipe_categories = require("./data/recipe_categories.json");
 const chefs = require("./data/chefs.json");
 
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Chef website is running");
+});
+
+app.get("/recipe_categories", (req, res) => {
+  res.send(recipe_categories);
 });
 
 app.get("/chefs", (req, res) => {
