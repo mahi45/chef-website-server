c871059bad80ea1a50ade0bf179b09d46a6d6b3f
Mohidul Hasan <mahi110145@gmail.com>
2023-05-03 23:21:53 +0600

    API changes for chef details

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const recipe_categories = require("./data/recipe_categories.json");
+const recipes = require("./data/recipe_categories.json");
 const chefs = require("./data/chefs.json");
 
 app.use(cors());
@@ -12,12 +12,18 @@ app.get("/", (req, res) => {
   res.send("Chef website is running");
 });
 
-app.get("/recipe_categories", (req, res) => {
-  res.send(recipe_categories);
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedChef = chefs.find((r) => r.chef_id === id);
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
